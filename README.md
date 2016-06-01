3a9638e31c93dfbd7b487097870953dda6aa15e2
churuxu <churuxu@126.com>
2016-06-01 19:23:39 +0800

    add gclient options

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+developing ...
 #travis-ci [![image](https://travis-ci.org/churuxu/webrtc_builds.svg?branch=master)](https://travis-ci.org/churuxu/webrtc_builds)
--- android-make.sh
+++ android-make.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 
-fetch --nohooks webrtc_android
-gclient sync
-ls
+df
+fetch --help
+fetch --nohooks --no-history webrtc_android || exit 1
+df
+gclient sync || exit 1
+df
+gclient runhooks || exit 1
+df
+
 
