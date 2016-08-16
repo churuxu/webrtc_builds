6842b03f4bbb7550aec54845fb74ff92b63552c8
churuxu <churuxu@126.com>
2016-08-16 17:09:38 +0800

    update [ci skip]

--- android-make.sh
+++ android-make.sh
@@ -3,8 +3,13 @@
 df -h
 fetch --help
 fetch --nohooks --no-history webrtc_android || exit 1
+echo ==================================================
+gclient help sync  
+echo ==================================================
+gclient help runhooks  
+alias git=mygit
 df -h
-gclient sync --no-history  || exit 1
+gclient sync -H  || exit 1
 df -h
 gclient runhooks --no-history  || exit 1
 df -h
