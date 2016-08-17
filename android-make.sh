981b4d56a23afc0c8f632c510be5e43a15d03a13
churuxu <churuxu@126.com>
2016-08-17 10:53:16 +0800

    test build

--- android-make.sh
+++ android-make.sh
@@ -1,25 +1,32 @@
 #/bin/bash
-ls $DEPOT_TOOLS_HOME
-df -h
-fetch --help
-echo ==================================================
-git --version
-alias git=mygit
-git --version
-fetch --nohooks --no-history breakpad || exit 1
-echo ==================================================
-gclient help sync  
-echo ==================================================
-gclient help runhooks  
-git --version
-alias git=mygit
-git --version
-df -h
-gclient sync -H --no-history || exit 1
-git --version
-alias
-df -h
-gclient runhooks --no-history  || exit 1
 df -h
 
+echo fetch ... 
+fetch --nohooks --no-history webrtc_android || exit 1
+df -h
+
+echo sync ... 
+gclient sync --no-history || exit 1
+df -h
+
+echo dir .:
+ls 
+
+echo dir trunk:
+ls trunk
+
+cd trunk
+source ./build/android/envsetup.sh
+export GYP_DEFINES="$GYP_DEFINES OS=android"
+df -h
+
+echo install deps ... 
+./build/install-build-deps.sh || exit 1
+df -h
+
+echo runhooks ... 
+gclient runhooks || exit 1
+
+echo build ... 
+ninja -C out/Release
 
