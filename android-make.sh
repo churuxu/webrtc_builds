274f99bdea5c929d37590a8b4d4683d48c0ffd86
churuxu <churuxu@126.com>
2016-08-17 09:53:53 +0800

    test git

--- android-make.sh
+++ android-make.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+ls $DEPOT_TOOLS_HOME
 df -h
 fetch --help
 fetch --nohooks --no-history breakpad || exit 1
@@ -7,9 +7,13 @@ echo ==================================================
 gclient help sync  
 echo ==================================================
 gclient help runhooks  
+git --version
 alias git=mygit
+git --version
 df -h
 gclient sync -H --no-history || exit 1
+git --version
+alias
 df -h
 gclient runhooks --no-history  || exit 1
 df -h
